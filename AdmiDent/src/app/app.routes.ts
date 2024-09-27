b143d0ac2d640f8472af03c249d8bfb11cdc1c17
Isaac Noriega <isaac.noriega@iteso.mx>
2024-09-26 18:39:55 -0600

    Descarga de plugins y path configurado

--- AdmiDent/src/app/app.routes.ts
+++ AdmiDent/src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { GalleryComponent } from './components/paths/home/gallery/gallery.component';
 import { HomeComponent } from './components/paths/home/home.component';
+import { ScheduleComponent } from './components/paths/home/schedule/schedule.component';
 
 
 export const routes: Routes = [
     {path : 'home'  , component : HomeComponent , children : [
-        { path : 'gallery' , component: GalleryComponent}
+        { path : 'gallery' , component: GalleryComponent},
+        { path : 'schedule' , component: ScheduleComponent}
     ]
     }
 ];
