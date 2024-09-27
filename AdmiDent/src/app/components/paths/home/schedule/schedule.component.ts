164a0911efa0c5da5519207911fa1483413d12ae
Isaac Noriega <isaac.noriega@iteso.mx>
2024-09-26 18:53:55 -0600

    lol

--- AdmiDent/src/app/components/paths/home/schedule/schedule.component.ts
+++ AdmiDent/src/app/components/paths/home/schedule/schedule.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { CalendarOptions } from '@fullcalendar/core'; // Importar desde @fullcalendar/core
 import dayGridPlugin from '@fullcalendar/daygrid'; // Importar el plugin para vista de cuadrícula diaria
 import interactionPlugin from '@fullcalendar/interaction'; // Importar el plugin para interacciones
-import { FullCalendarModule } from '@fullcalendar/angular'; // Importar FullCalendarModule
+import { FullCalendarModule } from '@fullcalendar/angular'; // Importar FullCalendarModuleimport timeGridPlugin from '@fullcalendar/timegrid';
 
 @Component({
   selector: 'app-schedule',
@@ -16,7 +16,7 @@ export class ScheduleComponent {
     plugins: [dayGridPlugin, interactionPlugin], // Registrar plugins
     initialView: 'dayGridMonth', // Vista inicial
     events: [
-      { title: 'Evento 1', date: '2024-09-28' },
+      { title: 'Evento 1', start: '2024-09-28',end:"2024-10-03", color:'red'},
       { title: 'Evento 2', date: '2024-09-29' }
     ],
     dateClick: this.handleDateClick.bind(this) // Manejar el clic en una fecha
