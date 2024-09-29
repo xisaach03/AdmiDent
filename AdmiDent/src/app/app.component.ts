fc50d748cb6ce1f345be94d7677078b7854d5218
XimeIsaac <Xih.12091957@gmail.com>
2024-09-28 20:19:24 -0600

    Commit diseño1
    This is the first design commit (navbar, welcome, register and login)

--- AdmiDent/src/app/app.component.ts
+++ AdmiDent/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from './components/layout/navbar/navbar.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- AdmiDent/src/app/app.routes.ts
+++ AdmiDent/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { WelcomeComponent } from './components/paths/welcome/welcome.component';
+import { LoginComponent } from './components/paths/login/login.component';
+import { RegisterComponent } from './components/paths/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'register', pathMatch: 'full'},
+    { path: 'welcome', component: WelcomeComponent},
+    { path: 'login', component: LoginComponent},
+    { path: 'register', component: RegisterComponent},
+
+];
