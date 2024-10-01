d18ff324ed4be7c4ddd6963b349ad60999d570e1
XimeIsaac <Xih.12091957@gmail.com>
2024-09-30 22:19:45 -0600

    Revert "Merge branch 'main' into X_branch"
    This reverts commit 3b3c4d500282f69dec551c69aab882d0d793637d, reversing
    changes made to fc50d748cb6ce1f345be94d7677078b7854d5218.

--- AdmiDent/src/app/app.routes.ts
+++ AdmiDent/src/app/app.routes.ts
@@ -2,16 +2,11 @@ import { Routes } from '@angular/router';
 import { WelcomeComponent } from './components/paths/welcome/welcome.component';
 import { LoginComponent } from './components/paths/login/login.component';
 import { RegisterComponent } from './components/paths/register/register.component';
-import { GalleryComponent } from './components/paths/home/gallery/gallery.component';
-import { HomeComponent } from './components/paths/home/home.component';
-import { ScheduleComponent } from './components/paths/home/schedule/schedule.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'register', pathMatch: 'full'},
     { path: 'welcome', component: WelcomeComponent},
     { path: 'login', component: LoginComponent},
     { path: 'register', component: RegisterComponent},
-    {path : 'home'  , component : HomeComponent , children : [
-        { path : 'gallery' , component: GalleryComponent},
-        { path : 'schedule' , component: ScheduleComponent}
-    ]
+
+];
--- AdmiDent/src/app/components/paths/home/home.component.ts
+++ AdmiDent/src/app/components/paths/home/home.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
