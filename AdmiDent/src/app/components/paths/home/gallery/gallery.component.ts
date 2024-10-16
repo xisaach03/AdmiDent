90d694d5dd1b4f23c0338b893a98ba07c812040a
Isaac Noriega <isaac.noriega@iteso.mx>
2024-10-16 10:13:48 -0600

    Funcion que verifica si el array esta vacio

--- AdmiDent/src/app/components/paths/home/gallery/gallery.component.ts
+++ AdmiDent/src/app/components/paths/home/gallery/gallery.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+interface treatment{
+  plan : string ,
+  url : string
+}
 @Component({
   selector: 'app-gallery',
   standalone: true,
@@ -9,4 +13,17 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './gallery.component.scss'
 })
 export class GalleryComponent {
+
+  img : treatment[] = [
+    // {plan: 'Imagen 1' , url : 'https://feji.us/acnawz'},
+    // {plan: 'Imagen 2' , url : 'https://feji.us/acnawz'},
+    // {plan: 'Imagen 3' , url : 'https://feji.us/acnawz'},
+    // {plan: 'Imagen 4' , url : 'https://feji.us/acnawz'},
+  ]
+
+  planEmpty(){
+return this.img.length === 0 ? true : false;
+  }
+
+
 }
