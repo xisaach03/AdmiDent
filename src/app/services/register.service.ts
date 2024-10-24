52069623d4cdb4ea2c0fafeebfd02a3241261613
ElCaffeesito <elcaffeesito777@gmail.com>
2024-10-24 11:19:02 -0600

    ya hay sistema (ya jala el registro)
    agregué el archivo y carpeta de invironments, pueden ver en register.service como accedo a ella y uso, cuando lo hagas no es necesario poner / después de la variable a la que accedes porque ya lo tiene, sino quedaría "//" en lugar de "/"

--- src/app/services/register.service.ts
+++ src/app/services/register.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, throwError } from 'rxjs';
+import { environment } from '../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +10,24 @@ export class RegisterService {
 
   constructor(private http: HttpClient) { }
 
-  registrar() {
-    return this.http.get('http://localhost:3000/home');
+  getUsers() {
+    return this.http.get<any>('http://localhost:3000/home');
+  }
+
+  registrar(data : string) {
+    const url = `${environment.apiUrl}register`
+    return this.http.post<any>(
+      url,
+      data
+    )
+    .pipe(
+      catchError(error => {
+        if (error.status !== 200) {
+          // Mostrar la alerta aquí, por ejemplo, usando un servicio de alertas o un simple alert()
+          alert(`Error al enviar los datos: ${error.status} - ${error.statusText}`);
+        }
+        return throwError(() => error); // Re-lanzar el error para que pueda ser manejado por otros suscriptores si es necesario
+      })
+    )
   }
 }
