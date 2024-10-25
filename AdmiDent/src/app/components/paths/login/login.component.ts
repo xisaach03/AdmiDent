6d976ab48899a89a1cf92ec60cdf311404425477
Ximena Isaac <Xih.12091957@gmail.com>
2024-10-24 21:46:23 -0600

    Jala pofi (login sin cache)

--- AdmiDent/src/app/components/paths/login/login.component.ts
+++ AdmiDent/src/app/components/paths/login/login.component.ts
@@ -1,12 +1,56 @@
 import { Component } from '@angular/core';
+import { LoginService } from '../../../services/login.service'; 
+import { FormBuilder, FormGroup, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
 
+  form: FormGroup;
+
+  constructor(
+    private loginService: LoginService, 
+    private formBuilder: FormBuilder,
+    private router: Router 
+  ) {
+    // Inicialización del formulario
+    this.form = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    });
+  }
+
+  login() {
+    if (this.form.valid) {
+      this.loginService.login(this.form.getRawValue()).subscribe({
+        next: (response) => {
+          //verifica si la respuesta es 'OK' por parte de la API gg
+          if (response === 'OK') {
+            console.log('Login exitoso:', response);
+            this.router.navigate(['/home']); //redirección a home
+          } else {
+            console.error('Respuesta inesperada:', response);
+            alert('Error inesperado al iniciar sesión.');
+          }
+        },
+        error: (err) => {
+          if (err.status === 401) {
+            alert('Credenciales incorrectas, por favor verifica tu email y contraseña.');
+          } else {
+            console.error('Error de login', err);
+            alert(`Error al iniciar sesión: ${err.statusText}`);
+          }
+        }
+      });
+    } else {
+      alert('Debes llenar todos los campos correctamente');
+    }
+  }
+  
 }
